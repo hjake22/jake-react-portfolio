9d02e9d50253073b820e5bf62187d3690510e4af
hjake22 <hjake@wustl.edu>
2024-06-11 18:47:55 -0500

    got rid of it

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+
 import LogoS from '../../../assets/images/logo-s.png'
 import './index.scss'
 
@@ -9,32 +9,7 @@ const Logo = () => {
   const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 0,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 3,
-        duration: 2,
-      }
-    )
-  }, [])
 
   return (
     <div className="logo-container" ref={bgRef}>
